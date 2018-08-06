81108169f768e92bc19fd54251ebce0e75f3eac9
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-08-06 16:42:38 -0500

    Music

---
+++ Cannons/Assets/Scripts/Audios/MusicAudios.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicAudios : MonoBehaviour {
+    [SerializeField] AudioSource musicAudiosource;
+    [SerializeField] AudioClip menuMusic;
+
+    private void Start()
+    {
+        MenuMusic();
+    }
+
+    public void MenuMusic() {
+        musicAudiosource.clip = menuMusic;
+        musicAudiosource.Play();
+    }
+
+}
