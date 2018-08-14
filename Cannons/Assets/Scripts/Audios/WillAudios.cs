930fb1f9bf78d6e193ec4653c132c05c15394e8e
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-08-13 21:38:48 -0500

    FixingAudios

--- Cannons/Assets/Scripts/Audios/WillAudios.cs
+++ Cannons/Assets/Scripts/Audios/WillAudios.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class WillAudios : MonoBehaviour
 {
     AudioClip[] willAudios;
+    AudioSource willA;
+    AudioMixer mixer;
+    string _OutputMixer = "Fx";
 
     private void Start()
     {
+        mixer = Resources.Load("Sounds/Master") as AudioMixer;
         willAudios = Resources.LoadAll<AudioClip>("Sounds/Fx/WillGoldTooth");
+        willA = GetComponent<AudioSource>();
+        willA.outputAudioMixerGroup = mixer.FindMatchingGroups(_OutputMixer)[0];
+        willA.playOnAwake = false;
     }
 
     public void DieAudio() {
-        GetComponent<AudioSource>().clip = willAudios[0];
-        GetComponent<AudioSource>().Play();
+        willA.clip = willAudios[0];
+        willA.Play();
     }
 }
