83079af16b5f82f4e62c7b42e1b328e29cb572f5
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-16 16:02:37 -0500

    Montón de mierda

---
+++ Cannons/Assets/Scripts/ChestAnimatedUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestAnimatedUI : MonoBehaviour {
+
+    Image image;
+    public Sprite[] sprites;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        StartCoroutine(SpriteAnim());
+    }
+
+    WaitForSeconds animSpeed = new WaitForSeconds(0.02f);
+
+    public IEnumerator SpriteAnim()
+    {
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            image.sprite = sprites[i];
+            yield return animSpeed;
+        }
+        StartCoroutine(SpriteAnim());
+    }
+}
--- Cannons/Assets/Scripts/Controllers/GameController/WinCondition.cs
+++ Cannons/Assets/Scripts/Controllers/GameController/WinCondition.cs
@@ -13,8 +13,11 @@ public class WinCondition : MonoBehaviour {
 
     Animator anim;
 
+    ParticleSystem cParticle;
+
     private void Start()
     {
+        cParticle = GetComponentInChildren<ParticleSystem>();
         anim = GetComponent<Animator>();
         mWriteVbles = (WriteVbles)FindObjectOfType(typeof(WriteVbles));
         mCanvasMgr = (CanvasMgr)FindObjectOfType(typeof(CanvasMgr));
@@ -23,21 +26,23 @@ public class WinCondition : MonoBehaviour {
     public void Win(Rigidbody _wills) {
         anim.SetBool("Opened", true);
         _wills.constraints = RigidbodyConstraints.FreezeAll;
-        StartCoroutine(OpeningChest());
+        StartCoroutine(ActivatingCanvas());
         CoinsMgr();
         if (level > Singleton.instance.LvlsUnlocked) {
             Singleton.instance.LvlsUnlocked = level;
-            //PlayerPrefs.SetInt("LvlUnlocked", Singleton.instance.LvlsUnlocked);
             Singleton.SaveUnlockLevels();
         }
     }
 
-    WaitForSeconds animLength = new WaitForSeconds(2f);//anim openChestLength
+    WaitForSeconds animLength = new WaitForSeconds(1.2f);//anim openChestLength
 
-    IEnumerator OpeningChest() {
+    IEnumerator ActivatingCanvas() {
+        yield return new WaitForSeconds(0.8f);
+        cParticle.Play();
         yield return animLength;
         mCanvasMgr.Canvas[0].SetActive(false);
         canvasWin.SetActive(true);
+        cParticle.Stop();
     }
 
     private void CoinsMgr() {
