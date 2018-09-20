70f674292c694fa3aeba269fafa160f0fa73c93e
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-19 23:19:29 -0500

    Stars

---
+++ Cannons/Assets/Scripts/Controllers/Lvls/Stars.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Stars : MonoBehaviour {
+
+    [SerializeField] float time3Stars, time2Stars, time1Star;
+    private float currentTime;
+
+    public static bool threeStars, twoStars, oneStar;
+
+    private void Start()
+    {
+        threeStars = false;
+        twoStars = false;
+        oneStar = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentTime = Time.timeSinceLevelLoad;
+        GiveStars(currentTime);
+    }
+
+    public void GiveStars(float _currentTime) {
+        if (_currentTime < time3Stars) {
+            threeStars = true;
+            print("Three Stars");
+        }
+        else if (_currentTime < time2Stars) {
+            twoStars = true;
+            print("Two Stars");
+        }
+        else if (_currentTime > time2Stars) {
+            oneStar = true;
+            print("One Star");
+        }
+    }
+}
---
+++ Cannons/Assets/Scripts/Controllers/Lvls/UnlockStars.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockStars : MonoBehaviour {
+
+    [SerializeField] Image[] imgStarsLocked;
+
+    private void Start()
+    {
+        if (Stars.threeStars)
+        {
+            foreach (Image a in imgStarsLocked) {
+                a.enabled = false;
+            }
+        }
+        else if (Stars.twoStars) {
+            for (int i = 0; i < 2; i++) {
+                imgStarsLocked[i].enabled = false;
+            }
+        }
+        else if (Stars.oneStar) {
+           imgStarsLocked[0].enabled = false;
+        }
+    }
+}
