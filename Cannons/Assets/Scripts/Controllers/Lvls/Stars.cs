f458af946c26d2c5864d936fd2c034259af74ac6
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-23 13:11:43 -0500

    StarsInGame

--- Cannons/Assets/Scripts/Controllers/Lvls/Stars.cs
+++ Cannons/Assets/Scripts/Controllers/Lvls/Stars.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Stars : MonoBehaviour {
+
+    [SerializeField] Image[] starsImg;
 
     [SerializeField] float time3Stars, time2Stars;
     private float currentTime;
@@ -24,17 +28,29 @@ public class Stars : MonoBehaviour {
         if (_currentTime < time3Stars) {
             Singleton.instance.Stars[mWinCondition.level - 1] = 3;
             print("Three Stars");
+            StartCoroutine(UnlockingStars(3));
         }
         else if (_currentTime < time2Stars) {
             if(Singleton.instance.Stars[mWinCondition.level -1] < 2)
                 Singleton.instance.Stars[mWinCondition.level - 1] = 2;
             print("Two Stars");
+            StartCoroutine(UnlockingStars(2));
         }
         else if (_currentTime > time2Stars) {
             if (Singleton.instance.Stars[mWinCondition.level - 1] < 1)
                 Singleton.instance.Stars[mWinCondition.level - 1] = 1;
             print("One Star");
+            StartCoroutine(UnlockingStars(1));
         }
         PlayerPrefsX.SetIntArray("Stars", Singleton.instance.Stars);
+    }
+
+    IEnumerator UnlockingStars(int _num) {
+        yield return new WaitForSeconds(3f);
+        for (int i = 0; i < starsImg.Length; i++) {
+            if (i < _num)
+                starsImg[i].enabled = false;
+            yield return new WaitForSeconds(1.4f);
+        }
     }
 }
