88c0ec21db6a33b1940f68f16702aee7b358720f
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-10-12 20:34:30 -0500

    AnimPanelShop

---
+++ Cannons/Assets/Scripts/Controllers/Shop/PanelShop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PanelShop : MonoBehaviour {
+
+    [SerializeField] Animator panelShopAnimator;
+
+    public void AnimPanelShopOpen() {
+        panelShopAnimator.SetBool("OpenShop", true);
+    }
+
+    public void AnimPanelShopClose() {
+        panelShopAnimator.SetBool("OpenShop", false);
+    }
+}
