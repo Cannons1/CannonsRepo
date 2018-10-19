4e2f4a302fc0fd2774d5b6a4bcc47cb1a1b4a2ca
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-10-18 22:13:28 -0500

    ButtonsCampaign(SoyDios)

--- Cannons/Assets/Scripts/Controllers/GameController/ScrollSnapRect.cs
+++ Cannons/Assets/Scripts/Controllers/GameController/ScrollSnapRect.cs
@@ -57,6 +57,13 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
     // container with Image components - one Image for each page
     private List<Image> _pageSelectionImages;
 
+    //inside class
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+
+    bool isActive = false;
+
     //------------------------------------------------------------------------
     void Start() {
         Time.timeScale = 1;
@@ -89,6 +96,9 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
 
         if (prevButton)
             prevButton.GetComponent<Button>().onClick.AddListener(() => { PreviousScreen(); });
+
+        if (_currentPage == 0)
+            prevButton.SetActive(false);
 	}
 
     //------------------------------------------------------------------------
@@ -112,6 +122,47 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
                 SetPageSelection(GetNearestPage());
             }
         }
+
+        if (isActive) {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //save began touch 2d point
+                firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                //save ended touch 2d point
+                secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                //create vector from the two points
+                currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                //normalize the 2d vector
+                currentSwipe.Normalize();
+
+                //swipe left
+                if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    NextScreen();
+                }
+                //swipe right
+                if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+                {
+                    PreviousScreen();
+                }
+            }
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        isActive = true;
+    }
+
+    private void OnDisable()
+    {
+        isActive = false;
     }
 
     //------------------------------------------------------------------------
@@ -225,11 +276,19 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
     //------------------------------------------------------------------------
     private void NextScreen() {
         LerpToPage(_currentPage + 1);
+        if (_currentPage != 0)
+            prevButton.SetActive(true);
+        if (_currentPage == _pageSelectionImages.Count - 1)
+            nextButton.SetActive(false);
     }
 
     //------------------------------------------------------------------------
     private void PreviousScreen() {
         LerpToPage(_currentPage - 1);
+        if (_currentPage == 0)
+            prevButton.SetActive(false);
+        if (_currentPage != _pageSelectionImages.Count - 1)
+            nextButton.SetActive(true);
     }
 
     //------------------------------------------------------------------------
@@ -302,3 +361,9 @@ public class ScrollSnapRect : MonoBehaviour, IBeginDragHandler, IEndDragHandler,
         }
     }
 }
+
+
+
+
+
+
