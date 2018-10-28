f23195ef8e6250a930dc7e4eda8b110b7591f114
Pirulei <danielpala@hotmail.es>
2018-10-27 19:23:54 -0500

    :balloon:

--- Cannons/Assets/AdMobManager.cs
+++ Cannons/Assets/AdMobManager.cs
@@ -7,16 +7,24 @@ using System;
 public class AdMobManager : MonoBehaviour {
 
     [SerializeField] string appID = "ca-app-pub-6196431305860923~5849023719";
-    [SerializeField] string rewardID = "ca - app - pub - 6196431305860923 / 2279360087";
+    [SerializeField] string rewardID = "ca-app-pub-3940256099942544/5224354917";
+    [SerializeField] string interstitialID = "";
     RewardBasedVideoAd rewardVideo;
+    InterstitialAd interstitial;
 
     private void Awake()
     {
-        MobileAds.Initialize(appID);
+        //MobileAds.Initialize(appID);
         rewardVideo = RewardBasedVideoAd.Instance;
+        interstitial = new InterstitialAd(interstitialID);
+
         rewardVideo.OnAdRewarded += OnRewardPlayer;
+        interstitial.OnAdClosed += InterstitialClosed;
+
         rewardVideo.OnAdClosed += RewardVideoClosed;
+
         RequestRewardAD();
+        RequestInterstitialAD();
     }
 
     private void OnRewardPlayer(object sender, EventArgs args)
@@ -25,11 +33,18 @@ public class AdMobManager : MonoBehaviour {
         Singleton.SaveCoins();
     }
 
-    private void RewardVideoClosed(object sender, EventArgs args) { RequestRewardAD(); }
+    private void InterstitialClosed(object sender, EventArgs e) { RequestInterstitialAD(); }
+    private void RewardVideoClosed(object sender, EventArgs e) { RequestRewardAD(); }
  
+    private void RequestInterstitialAD()
+    {
+        AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).Build();
+        interstitial.LoadAd(request);
+    }
+
     private void RequestRewardAD()
     {
-        AdRequest request = new AdRequest.Builder().Build();
+        AdRequest request = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).Build();
         rewardVideo.LoadAd(request, rewardID);        
     }
 
@@ -42,6 +57,18 @@ public class AdMobManager : MonoBehaviour {
         else
         {
             RequestRewardAD();
+        }
+    }
+
+    public void ShowInterstitialAD()
+    {
+        if(interstitial.IsLoaded())
+        {
+            interstitial.Show();
+        }
+        else
+        {
+            RequestInterstitialAD();
         }
     }
 }
