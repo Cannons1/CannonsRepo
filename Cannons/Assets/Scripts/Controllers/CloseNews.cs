cc9fe4de3a006e2cfd11f258ac101e0533c6ef83
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-11-02 10:38:52 -0500

    PanelNewwwwwwss

---
+++ Cannons/Assets/Scripts/Controllers/CloseNews.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CloseNews : MonoBehaviour {
+    [SerializeField] Animator animatorNews;
+
+    int isActive;
+
+    private void Start()
+    {
+        //PlayerPrefs.DeleteKey("News");
+        if (PlayerPrefs.HasKey("News"))
+        {
+            isActive = PlayerPrefs.GetInt("News");
+            if (isActive == 0)
+                gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void SetAnimatorClosed()
+    {
+        animatorNews.SetBool("Closed", true);
+        PlayerPrefs.SetInt("News", 0);
+    }
+}
