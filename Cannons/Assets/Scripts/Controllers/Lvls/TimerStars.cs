3482cac10e35546c6f23228494758f080d8caff3
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-11-01 19:20:28 -0500

    Estrellas

--- Cannons/Assets/Scripts/Controllers/Lvls/TimerStars.cs
+++ Cannons/Assets/Scripts/Controllers/Lvls/TimerStars.cs
@@ -5,60 +5,46 @@ using UnityEngine.UI;
 public class TimerStars : MonoBehaviour
 {
     [SerializeField] Stars stars;
-    [SerializeField] Image[] starsImgs;
     [SerializeField] AudioController audioController;
     [SerializeField] WinCondition winCondition;
+    [SerializeField] Animator starsAnimator;
 
     float time = 0f;
-
-    bool canDesapearThird = false, canDesapearScnd = false;
-
-    private void Start()
-    {
-        StartCoroutine(ActiveStars());
-    }
-
-    IEnumerator ActiveStars() {
-        yield return new WaitForSeconds(2.3f);
-        foreach (Image star in starsImgs)
-        {
-            star.enabled = true;
-            yield return null;
-        }
-    }
+    bool canDesapearThird = true, canDesapearScnd = true;
 
     private void Update()
     {
         time += Time.deltaTime;
         if (time > stars.time3Stars - 3f && time < stars.time3Stars)
         {
-            if (!canDesapearThird)
-                StartCoroutine(DesapearStar(starsImgs.Length -1, stars.time3Stars));
-            canDesapearThird = true;
+            if (canDesapearThird)
+                StartCoroutine(DesapearStar(3, stars.time3Stars));
+            canDesapearThird = false;
         }
 
         if (time > stars.time2Stars - 3f && time < stars.time2Stars)
         {
-            if (!canDesapearScnd)
-                StartCoroutine(DesapearStar(starsImgs.Length - 2, stars.time2Stars));
-            canDesapearScnd = true;
+            if (canDesapearScnd)
+                StartCoroutine(DesapearStar(2, stars.time2Stars));
+            canDesapearScnd = false;
         }
     }
 
+    WaitForSeconds wait = new WaitForSeconds(0.5f);
+
     IEnumerator DesapearStar(int _star, float _timeStars)
     {
+        starsAnimator.SetInteger("Star", _star);
         while (time < _timeStars && !winCondition.WinBool)
         {
-            starsImgs[_star].enabled = false;
-            yield return new WaitForSeconds(0.5f);
+            yield return wait;
             audioController.AudioStar(0.5f);
-            starsImgs[_star].enabled = true;
-            yield return new WaitForSeconds(0.5f);
+            yield return wait;
         }
+        starsAnimator.SetInteger("Star", 0);
         if (!winCondition.WinBool) {
             audioController.AudioStar(0.5f);
         }
         audioController.ItemAudioSource.volume = 1f;
-        starsImgs[_star].enabled = false;
     }
 }
